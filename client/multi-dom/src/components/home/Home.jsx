f5e18ac3b806d4c5d1cb1e65ef4b12d463351df8
Denis Atanasov <denisatanasov76@gmail.com>
2025-04-27 12:50:00 +0300

    create Home page

---
+++ client/multi-dom/src/components/home/Home.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router";
+import Slider from "./img-slider/Slider";
+import Advantages from "./advantages/Advantages";
+import BestSellers from "./best-sellers/BestSellers";
+import NewProducts from "./new-products/NewProducts";
+import SelectedProducts from "./selected-products/SelectedProducts";
+import Welcome from "./welcome/Welcome";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col">
+      <Slider />
+      <Welcome />
+      <BestSellers />
+      <NewProducts />
+      <SelectedProducts />
+      <Advantages />
+    </div>
+  );
+}
