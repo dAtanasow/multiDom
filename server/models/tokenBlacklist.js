a25202c9e9a175e45fbcd287788a23923f850c24
Denis Atanasov <denisatanasov76@gmail.com>
2025-04-28 11:34:03 +0300

    create tokenBlackList model

---
+++ server/models/tokenBlacklist.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const tokenBlacklistSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: true,
+    },
+    blacklistedAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const TokenBlacklist = mongoose.model('TokenBlacklist', tokenBlacklistSchema);
+
+export default TokenBlacklist;
