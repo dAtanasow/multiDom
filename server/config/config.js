a69e92d6386ed2ddd72b875b763023b83e6e5c66
Denis Atanasov <denisatanasov76@gmail.com>
2025-04-28 13:45:17 +0300

    add config

--- server/config/config.js
+++ server/config/config.js
@@ -3,12 +3,19 @@ const env = process.env.NODE_ENV || 'development';
 const config = {
     development: {
         port: process.env.PORT || 3000,
-        origin: ['http://localhost:5173']
+        dbURL: process.env.MONGO_URI || 'mongodb://localhost:27017/МултиДом',
+        cookieSecret: process.env.COOKIESECRET,
+        jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
+        jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+        origin: ['http://localhost:5173'],
     },
     production: {
         port: process.env.PORT || 3000,
         dbURL: process.env.DB_URL_CREDENTIALS,
-        origin: []
+        cookieSecret: process.env.COOKIESECRET,
+        jwtAccessSecret: process.env.JWT_ACCESS_SECRET,
+        jwtRefreshSecret: process.env.JWT_REFRESH_SECRET,
+        origin: [process.env.FRONTEND_URL],
     }
 };
 
