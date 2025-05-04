baf5664a1c2ec46f3f6892884b7e5f99d832573a
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-04 09:23:42 +0300

    create product model

---
+++ server/models/Product.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    manufacturer: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+    },
+    subCategory: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    discountPrice: {
+        type: Number,
+    },
+    quantity: {
+        type: Number,
+        default: 0,
+    },
+    unitCount: {
+        type: Number,
+    },
+    unitType: {
+        type: String,
+    },
+    images: {
+        type: [String],
+    },
+    originCountry: {
+        type: String,
+    },
+    isFeatured: {
+        type: Boolean,
+        default: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+const Product = mongoose.model('Product', productSchema);
+module.exports = Product;
