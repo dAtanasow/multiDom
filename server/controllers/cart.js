504072e7125feb60452248daca806670691f6c94
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-05 16:11:36 +0300

    create cart controller

---
+++ server/controllers/cart.js
@@ -0,0 +1,61 @@
+const Cart = require("../models/Cart");
+
+const getCart = async (req, res, next) => {
+    try {
+        const cart = await Cart.findOne({ user: req.user._id }).populate('items.product');
+        res.status(200).json(cart || { user: req.user._id, items: [] });
+    } catch (err) {
+        next(err);
+    }
+};
+
+const updateCart = async (req, res, next) => {
+    try {
+        const { items } = req.body;
+        let cart = await Cart.findOneAndUpdate(
+            { user: req.user._id },
+            { user: req.user._id, items },
+            { upsert: true, new: true }
+        );
+        res.status(200).json(cart);
+    } catch (err) {
+        next(err);
+    }
+};
+
+const deleteCartItem = async (req, res, next) => {
+    try {
+        const { productId } = req.params;
+        const cart = await Cart.findOneAndUpdate(
+            { user: req.user._id },
+            { $pull: { items: { product: productId } } },
+            { new: true }
+        ).populate("items.product");
+
+        res.status(200).json(cart);
+    } catch (err) {
+        next(err);
+    }
+};
+
+const clearCart = async (req, res, next) => {
+    try {
+        const cart = await Cart.findOneAndUpdate(
+            { user: req.user._id },
+            { items: [] },
+            { new: true }
+        ).populate("items.product");
+
+        res.status(200).json(cart);
+    } catch (err) {
+        next(err);
+    }
+};
+
+
+module.exports = {
+    getCart,
+    updateCart,
+    deleteCartItem,
+    clearCart,
+};
