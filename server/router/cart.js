8af5eddd027f157b5648ef8b87d5b595d98f0893
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-06 08:19:42 +0300

    add auth guard to cart paths

--- server/router/cart.js
+++ server/router/cart.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { getCart, deleteCartItem, updateCart, clearCart, addToCart } = require('../controllers/cart');
+const { getCart, deleteCartItem, updateCart, clearCart, addToCart }
+    = require('../controllers/cart');
+const { auth } = require('../utils');
 
 const router = express.Router();
 
-router.get('/', getCart)
-router.put('/', updateCart)
-router.delete('/', clearCart);
-router.post('/add', addToCart); 
-router.delete('/:productId', deleteCartItem)
+router.get('/', auth(), getCart)
+router.put('/', auth(), updateCart)
+router.delete('/', auth(), clearCart);
+router.post('/add', auth(), addToCart);
+router.delete('/:productId', auth(), deleteCartItem)
 
 module.exports = router;
