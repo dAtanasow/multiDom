5036404a2496ef4808620171b6d31c74fda935cd
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-06 16:10:04 +0300

    change table data and styling in ProductsTable

--- client/multi-dom/src/components/admin/products-admin/ProductsTable.jsx
+++ client/multi-dom/src/components/admin/products-admin/ProductsTable.jsx
@@ -13,31 +13,42 @@ export default function ProductsTable({ setProductView, setEditingProduct }) {
 
 
     return (
-        <div className="p-6 max-w-4xl mx-auto">
-            <table className="w-full border text-sm">
+        <div className="pb-6 pt-6 max-w-4xl mx-auto">
+            <table className="w-full text-center justify-center border text-sm">
                 <thead>
                     <tr className="bg-gray-100">
                         <th className="border px-2 py-1">Снимка</th>
                         <th className="border px-2 py-1">Име</th>
                         <th className="border px-2 py-1">Цена</th>
-                        <th className="border px-2 py-1">Категория</th>
-                        <th className="border px-2 py-1">Подкатегория</th>
+                        <th className="border px-2 py-1">Промо</th>
+                        <th className="border px-2 py-1">Налични</th>
                         <th className="border px-2 py-1">Действия</th>
                     </tr>
                 </thead>
                 <tbody>
                     {products.map((product) => (
                         <tr key={product._id}>
-                            <td className="border px-2 py-1">
-                                <img src={product.images?.[0]} alt={product.name} className="h-12 w-12 object-contain" />
+                            <td className="border px-2 py-1 align-middle">
+                                <div className="w-24 h-18 mx-auto">
+                                    <img
+                                        src={product.images?.[0]}
+                                        alt={product.name}
+                                        className="w-full h-full object-contain"
+                                    />
+                                </div>
                             </td>
                             <td className="border px-2 py-1">{product.name}</td>
-                            <td className="border px-2 py-1">{product.price?.toFixed(2)} лв.</td>
-                            <td className="border px-2 py-1">{product.category}</td>
-                            <td className="border px-2 py-1">{product.subCategory}</td>
-                            <td className="border px-2 py-1 space-x-2">
-                                <button className="bg-yellow-500 text-white px-2 py-1 rounded hover:bg-yellow-600" onClick={() => handleEditClick(product)}>Редактирай</button>
-                                <button className="bg-red-600 text-white px-2 py-1 rounded hover:bg-red-700" onClick={() => handleDelete(product._id)}>Изтрий</button>
+                            <td className="border px-2 py-1">{product.price?.toFixed(2)}</td>
+                            <td className={`border px-2 py-1 ${product.discountPrice ? "text-green-600 font-semibold" : ""}`}>
+                                {product.discountPrice ? product.discountPrice.toFixed(2) : "-"}
+                            </td>
+                            <td className={`border px-2 py-1 ${product.quantity < 10 ? "bg-yellow-100 text-yellow-800 font-semibold" : ""}`}>
+                                {product.quantity}
+                            </td>
+
+                            <td className="border space-x-2">
+                                <button className="w-2/3 max-w-25 bg-yellow-500 m-1 text-white p-1 rounded hover:bg-yellow-600" onClick={() => handleEditClick(product)}>Редактирай</button>
+                                <button className="w-2/3 max-w-25 bg-red-600 m-1 text-white p-1 rounded hover:bg-red-700" onClick={() => handleDelete(product._id)}>Изтрий</button>
                             </td>
                         </tr>
                     ))}
