dd2fe17fb7ab855f0ea0d16927f2ffcf9d6c24c9
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-07 09:09:01 +0300

    create get new added products api

--- client/multi-dom/src/api/catalog.js
+++ client/multi-dom/src/api/catalog.js
@@ -6,9 +6,12 @@ const getAll = (query = "") => requester.get(`${baseUrl}${query ? `?${query}` : 
 
 const getById = (id) => requester.get(`${baseUrl}/${id}`);
 
+const getNewest = () => getAll("sortBy=createdAt&order=desc");
+
 const catalogApi = {
     getAll,
     getById,
+    getNewest,
 };
 
 export default catalogApi;
