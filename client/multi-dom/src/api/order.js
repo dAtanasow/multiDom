d5d332beea7fe9f1885dec5d7a1e46a6053bae61
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-14 11:05:14 +0300

    add order fetch api

--- client/multi-dom/src/api/order.js
+++ client/multi-dom/src/api/order.js
@@ -1,13 +1,28 @@
 import requester from "./requester";
 
-const baseUrl = import.meta.env.VITE_API_URL + '/thank-you';
+const baseUrl = import.meta.env.VITE_API_URL + "/order";
 
 const createOrder = (data) => {
-    return requester.post(baseUrl, data);
-}
+    return requester.post(`${baseUrl}`, data);
+};
+
+const getOrderById = (id) => {
+    return requester.get(`${baseUrl}/${id}`);
+};
+
+const getAllOrders = () => {
+    return requester.get(baseUrl);
+};
+
+const updateOrderStatus = (id, status) => {
+    return requester.put(`${baseUrl}/${id}/status`, { status });
+};
 
 const orderApi = {
     createOrder,
+    getOrderById,
+    getAllOrders,
+    updateOrderStatus,
 };
 
 export default orderApi;
