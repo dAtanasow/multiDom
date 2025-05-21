1bc9b51cfa31bbc9b419c121775a8d0ea885b782
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-21 23:38:35 +0300

    create toggleFavorite route and controller

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -140,9 +140,35 @@ const editProfile = async (req, res, next) => {
     }
 };
 
+const toggleFavorite = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const { productId } = req.body;
+
+    const user = await User.findById(userId);
+
+    if (!user) return res.status(404).json({ message: 'Потребителят не е намерен' });
+
+    const index = user.favorites.indexOf(productId);
+
+    if (index > -1) {
+      user.favorites.splice(index, 1);
+    } else {
+      user.favorites.push(productId);
+    }
+
+    await user.save();
+    res.json({ favorites: user.favorites });
+  } catch (err) {
+    res.status(500).json({ message: 'Грешка при запазване на любими' });
+  }
+};
+
+
 module.exports = {
     getUserAddresses,
     addUserAddress,
     deleteUserAddress,
-    editProfile
+    editProfile,
+    toggleFavorite,
 };
--- server/router/profile.js
+++ server/router/profile.js
@@ -1,12 +1,13 @@
 
 const express = require('express');
 const router = express.Router();
-const { editProfile, getUserAddresses, addUserAddress, deleteUserAddress } = require("../controllers/profile");
+const { editProfile, getUserAddresses, addUserAddress, deleteUserAddress, toggleFavorite } = require("../controllers/profile");
 const auth = require("../utils/auth");
 
 router.get("/addresses", auth(), getUserAddresses);
 router.post("/addresses", auth(), addUserAddress);
 router.delete("/addresses/:index", auth(), deleteUserAddress);
+router.post('/favorites/toggle', auth(), toggleFavorite);
 router.put('/edit', auth(), editProfile);
 
 module.exports = router;
