c354af6f1528c01e5cbd8811df4bca291efd4526
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-25 13:33:08 +0300

    implement cartSync when login  and local clearCart when logout

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -3,6 +3,9 @@ import { toast } from "react-toastify";
 import userApi from "../api/auth";
 import { useAuthContext } from "../context/AuthContext";
 import { useNavigate } from "react-router";
+import { setAccessToken } from "../utils/authUtil";
+import { useCartContext } from "../context/CartContext";
+import { syncLocalCartToServer } from "../utils/cartSync";
 
 export function useRegister() {
     const [loading, setLoading] = useState(false);
@@ -28,14 +31,14 @@ export function useRegister() {
 export function useLogin() {
     const [loading, setLoading] = useState(false);
     const { changeAuthState } = useAuthContext();
+    const { setCart, syncedRef } = useCartContext();
 
     const login = async (loginData) => {
         setLoading(true);
         try {
             const result = await userApi.login(loginData);
             const { user, accessToken } = result;
-
-
+            setAccessToken(accessToken);
             changeAuthState({
                 user: {
                     _id: user._id,
@@ -47,7 +50,7 @@ export function useLogin() {
                 },
                 accessToken: accessToken,
             });
-
+            await syncLocalCartToServer(setCart, syncedRef);
             toast.success("Успешен вход!");
             return result;
         } catch (error) {
@@ -65,11 +68,13 @@ export function useLogin() {
 export function useLogout() {
     const navigate = useNavigate();
     const { logout, isLoggingOut } = useAuthContext();
+    const { clearCart } = useCartContext();
 
     const logoutHandler = async () => {
         if (isLoggingOut) return;
 
         try {
+            clearCart({ localOnly: true });
             await logout();
             toast.success("Успешно излязохте от профила.");
             navigate("/", { replace: true });
