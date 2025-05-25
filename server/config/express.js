8a22439f7d91b142801e226f3c7ae63e18ec21e2
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-25 13:39:32 +0300

    change cors settings

--- server/config/express.js
+++ server/config/express.js
@@ -6,13 +6,6 @@ const cookieSecret = process.env.COOKIESECRET;
 module.exports = (app) => {
     app.use(express.json({ limit: "10mb" }));
 
-    app.use((req, res, next) => {
-        if (req.method === 'GET') {
-            req.body = undefined;
-        }
-        next();
-    });
-
     app.use(cookieParser(cookieSecret));
 
     app.use(express.static(path.resolve(__basedir, 'static')));
--- server/server.js
+++ server/server.js
@@ -1,28 +1,23 @@
 global.__basedir = __dirname;
 require('dotenv').config()
-const dbConnector = require('./config/db');
-const apiRouter = require('./router');
 const cors = require('cors');
 const express = require('express');
+const dbConnector = require('./config/db');
+const apiRouter = require('./router');
+const config = require('./config/config');
 
 dbConnector()
     .then(() => {
-        const config = require('./config/config');
-        console.log('✅ ENV:', process.env.NODE_ENV);
-        console.log('✅ DB URL:', config.dbURL);
-        console.log("✅ ENV MONGO_URI:", process.env.MONGO_URI);
-
-
         const app = express();
-        require('./config/express')(app);
-
+        
         app.use(cors({
             origin: config.origin,
             credentials: true,
-            allowedHeaders: ['Content-Type', 'Authorization'],
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
         }));
-
+        require('./config/express')(app);
+        
         app.use('/api', apiRouter);
 
         app.listen(config.port, () => {
