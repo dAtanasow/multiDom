4355a28d5bbba82e80589057680d6ca4f0e7dce2
Denis Atanasov <denisatanasov76@gmail.com>
2025-05-26 21:52:58 +0300

    fix path bug

--- server/router/review.js
+++ server/router/review.js
@@ -17,14 +17,12 @@ const {
 } = require("../controllers/review");
 const { auth } = require("../utils");
 
-router.get("/:productId", getReviewsByProductId);
 
 router.post("/", auth(), addReview);
 router.put("/", auth(), updateReview);
 router.put("/:reviewId/like", auth(), likeReview);
 router.put("/:reviewId/dislike", auth(), dislikeReview);
 router.post("/:reviewId/comments", auth(), addComment);
-router.delete("/:reviewId", auth(), deleteReview);
 
 router.put("/:reviewId/comments/:commentId/like", auth(), likeComment);
 router.put("/:reviewId/comments/:commentId/dislike", auth(), dislikeComment);
@@ -33,5 +31,9 @@ router.delete("/:reviewId/comments/:commentId", auth(), deleteComment);
 
 router.put('/:reviewId/comments/:commentId/replies/:replyId/like', auth(), likeReply);
 router.put('/:reviewId/comments/:commentId/replies/:replyId/dislike', auth(), dislikeReply);
+
+router.delete("/:reviewId", auth(), deleteReview);
+
+router.get("/product/:productId", getReviewsByProductId);
 
 module.exports = router;
