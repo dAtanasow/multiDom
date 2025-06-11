60db838fd5b06a427f64c243a9be60cb28744984
Denis Atanasov <denisatanasov76@gmail.com>
2025-06-11 08:57:03 +0300

    handle EMAIL_CONFIRMED message and auto-login after email confirmation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,20 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import MobileHeaderContent from "./mobile/MobileHeaderContent";
 import DesktopHeaderContent from "./desktop/DesktopHeaderContent";
 import PanelSwitcher from "./PanelSwitcher";
 import { useIsMobile } from "../../hooks/useResponsive";
 import { usePanel } from "../../hooks/usePanel";
+import { toast } from "sonner";
+import { setAccessToken } from "../../utils/authUtil";
+import { useAuthContext } from "../../context/AuthContext";
+import userApi from "../../api/auth";
 
 export default function Header() {
   const isMobile = useIsMobile();
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
+  const { changeAuthState } = useAuthContext();
 
   const { panel: activePanel, setActivePanel, closePanel } = usePanel();
+  useEffect(() => {
+    const handleMessage = async (event) => {
+      if (event.origin !== window.location.origin) return;
+
+      if (event.data?.type === "EMAIL_CONFIRMED") {
+        const token = event.data.accessToken;
+        setAccessToken(token);
+        try {
+          const user = await userApi.getCurrentUser();
+          changeAuthState({ user, accessToken: token });
+          toast.success("Имейлът е потвърден и сте логнати.");
+        } catch (err) {
+          toast.error("Грешка при вход.");
+          console.log(err.message);
+
+        }
+      }
+    };
+
+    window.addEventListener("message", handleMessage);
+    return () => window.removeEventListener("message", handleMessage);
+  }, [changeAuthState]);
 
   return (
-    <header role="banner" className="bg-white shadow-md sticky top-0 z-50">
+    <header className="fixed top-0 left-0 w-full h-[64px] z-50 bg-white shadow">
       {isMobile ? (
         <MobileHeaderContent
           setMobileMenuOpen={setMobileMenuOpen}
